48c67ba96d7ed4230845ddf28b6e5021d3cf694e
clamar92 <cla.mar92@gmail.com>
2025-06-18 12:53:05 +0200

    commit

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 from app import create_app, db
 
-app = create_app(config_name="testing")
+app = create_app()
 import os
 with app.app_context():
     db.create_all()
-    print("Tabelle create con successo.")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
